f8d65f70db4cc5b83bc92f0794e9a45da645f9ad
Ramz <s_ramis@inbox.ru>
2022-04-11 16:31:28 +0500

    delete main2

--- src/uz/epam/BallRunner.java
+++ src/uz/epam/BallRunner.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class BallRunner {
 
     public static void main(String[] args) throws IOException {
         Path path = Path.of("src/resources/ballTest.txt");
